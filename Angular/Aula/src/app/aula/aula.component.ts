a5b194b8b8f800a9900671a90e01bffd0cd7cb77
itscamicaro <145448027+itscamicaro@users.noreply.github.com>
2024-05-09 21:02:20 +0200

    Creando aulacomponent css y ts

---
+++ Angular/Aula/src/app/aula/aula.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aula',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      aula works!
+    </p>
+  `,
+  styleUrl: './aula.component.css'
+})
+export class AulaComponent {
+
+}
